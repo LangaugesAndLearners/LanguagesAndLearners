11daf38cdf4a78665de1b87bd2a98a5f464dcd5b
Steve Johnson <steve21johnson@gmail.com>
2018-10-21 13:02:41 -0700

    reduced to showing 3 vocab words

--- convo.js
+++ convo.js
@@ -1,5 +1,33 @@
 var data =
 [{
+ promptText: "Nurse asking doctor for a leave",
+ youAreImgSrc: "https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?size=338&ext=jpg",
+ youAreText: "the nurse",
+ vocab: [
+   {
+     vocabWord: "vacation",
+     vocabText: "有休",
+     vocabImgSrc: "https://image.shutterstock.com/image-photo/anatomical-tweezers-isolated-on-white-260nw-529351978.jpg",
+   },
+   {
+     vocabWord: "stress",
+     vocabText: "ストレス",
+     vocabImgSrc: 'https://media.istockphoto.com/photos/thermometer-set-isolated-on-white-picture-id526560402?k=6&m=526560402&s=612x612&w=0&h=5c5FBoZe4QVA9MlomDbT_2UpTfx3j0NqxjdfIg4Eh6g=',
+   },
+   {
+     vocabWord: "migraine",
+     vocabText: "頭が痛い",
+     vocabImgSrc: 'https://images-na.ssl-images-amazon.com/images/I/91ZkmwMJbWL._AC_SS350_.jpg',
+   },
+   {
+     vocabWord: "medicine",
+     vocabText: "薬",
+     vocabImgSrc: 'https://cdn.shopify.com/s/files/1/0535/3509/products/Black_Pen_1024x1024_f2fbd04b-d2a3-4ba2-ba3c-508ef4e65ad2_large.jpg?v=1502887399',
+   },
+ ],
+}]
+var dataDummy =
+[{
   promptText: "You're feeling sick, so you head to the doctor's office",
   youAreImgSrc: "https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?size=338&ext=jpg",
   youAreText: 'Things go from bad to worse: you turn into a zombie...',
@@ -70,7 +98,7 @@ function update(targetId) {
   })
 
   $("#" + jqTarget.attr('id') + " i.fa-check-circle").on("click", function(){
-    console.log($(this).parent().attr('id'))
+    //console.log($(this).parent().attr('id'))
     var next =$(this).parent().attr('id')
     update(next)
   })
@@ -79,4 +107,3 @@ function update(targetId) {
 update("a")
 update('b')
 update('c')
-update('d')
